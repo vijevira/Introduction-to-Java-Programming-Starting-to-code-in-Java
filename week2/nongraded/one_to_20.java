ddc3c57e749b2924ef44d69664e82ab29337e071
Vijendra Kumar <43384082+vijevira@users.noreply.github.com>
2019-04-23 13:21:10 +0530

    Create one_to_20.java

---
+++ week2/nongraded/one_to_20.java
@@ -0,0 +1,17 @@
+/**
+ * Do not change the method or class
+ * declarations.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i=0; i < 21; i++){
+		    System.out.print(i);
+		    if(i<20){
+		        System.out.print(", ");
+		    }
+		}
+		System.out.println(".");
+	}
+}
